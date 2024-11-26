c6df754ade297514b6bf4070fb2253cf9b16c449
SILVANA FRANCISCO ALVES <132940036+silvaninha007@users.noreply.github.com>
2024-11-26 08:39:17 -0300

    Add files via upload

---
+++ Flashcard -- Silvana/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?', 
+    'A capital da França é Paris.'
+)
+
+criaCartao (
+    'Taylor Swift',
+    'Qual a melhor música dela?', 
+    'Wildest Dreams.'
+)
+
+criaCartao (
+    'Futebol',
+    'Quem é o melhor jogador do mundo (atualmente)?', 
+    'Vini Jr.'
+)
